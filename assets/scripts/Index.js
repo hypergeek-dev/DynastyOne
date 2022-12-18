dc5f072cc1882a240dbc9e59fb639b7d9af1a499
hypergeek-dev <32468376+hypergeek-dev@users.noreply.github.com>
2022-12-18 15:11:14 +0000

    js for headline

--- assets/scripts/Index.js
+++ assets/scripts/Index.js
@@ -62,3 +62,11 @@ function play() {
   audio.currentTime = 0
   audio.play()
 }
+//variable for headline on landingpage
+
+{
+  document.getElementById("headline");
+  var tl = gsap.timeline({ repeat: -1 });
+  tl.to("h1", 200, { backgroundPosition: "-960px 0" });
+}
+
